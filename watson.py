8f29e952f28a2ea1c0926718c4fdf5c7bfd97011
7GS <t.sargeantson@gmail.com>
2021-04-01 14:08:21 -0600

    second commit

--- watson.py
+++ watson.py
@@ -12,8 +12,19 @@ from settings import *
 df = pd.read_csv('data_modified.csv', index_col='playerId')
 df = df.round(0)
 
+# Create draft results data frame
+results_df = pd.read_csv('draft_results.csv', index_col='round')
+
 ## Draw sidebar
-st.sidebar.image('terminator.png', width = 55)
+
+col1, col2 = st.sidebar.beta_columns(2)
+with col1:
+    st.sidebar.image('terminator.png', width = 55)
+
+with col2:
+    menu_selection = st.sidebar.radio('Menu:', ('Players', 'Teams'))
+
+
 playerIds = df.index.values.tolist()
 players = df['player'].tolist()
 playerDic = dict(zip(playerIds, players))
@@ -28,36 +39,52 @@ st.sidebar.write("<b>>> Price target: $" + str(df.at[player_choice, 'price']) + 
 st.sidebar.write("")
 
 drafter = st.sidebar.selectbox("Choose a manager:", managers)
-price = st.sidebar.number_input("Enter price:", min_value=1, max_value=200)
-st.sidebar.button("Submit Draft Pick")
+auction_price = st.sidebar.number_input("Enter price:", min_value=1, max_value=200)
+
+if st.sidebar.button("Submit Draft Pick"):
+    results_df.loc[len(results_df.index)] = [player_choice, df.at[player_choice, 'player'], df.at[player_choice, 'team'], df.at[player_choice, 'position'], df.at[player_choice, 'fpts'], df.at[player_choice, 'vab'], df.at[player_choice, 'price'], auction_price, drafter]
+    results_df.to_csv('draft_results.csv')
+
+
+with st.sidebar.beta_expander("Adjust settings"):
+    st.write("Test")
+    if st.sidebar.button("Reset Draft"):
+        results_df = results_df[df.round == 0]
+        results_df.to_csv('draft_results.csv')
 
 ## Draw Main Page
-positions = df['position'].unique()
-position_choice = st.selectbox("Position to view:", positions)
 
-df = df[df.position == position_choice]
-df = df.sort_values('fpts', ascending=False)
+if menu_selection == 'Players':
+    positions = df['position'].unique()
+    position_choice = st.selectbox("Position to view:", positions)
 
-st.dataframe(df[['player', 'team', 'position', 'fpts', 'vab', 'price']])
+    df = df[df.position == position_choice]
+    df = df.sort_values('fpts', ascending=False)
 
-source = pd.DataFrame({
-    'x': df['position'],
-    'y': df['vab'],
-    'label': df['player']
-})
+    st.dataframe(df[['player', 'team', 'position', 'fpts', 'vab', 'price']])
 
-points = alt.Chart(source).mark_point().encode(
-    x='x:N',
-    y='y:Q'
-)
+    newdf = df[df.vab > 0]
 
-text = points.mark_text(
-    align='left',
-    baseline='middle',
-    dx=7,
-    color='white'
-).encode(
-    text = 'label'
-)
+    source = pd.DataFrame({
+        'x': newdf['position'],
+        'y': newdf['vab'],
+        'label': newdf['player']
+    })
 
-st.altair_chart(points + text, use_container_width=True)
+    points = alt.Chart(source).mark_point().encode(
+        x='x:N',
+        y='y:Q'
+    )
+
+    text = points.mark_text(
+        align='left',
+        baseline='middle',
+        dx=7,
+        color='white'
+    ).encode(
+        text = 'label'
+    )
+
+    st.altair_chart(points + text, use_container_width=True)
+elif menu_selection == 'Teams':
+    st.dataframe(results_df)
